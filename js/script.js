a84891934fe0fa030b37c61a0605c53bb1c315a5
chuck4182 <chuck4182@yahoo.com>
2015-01-30 06:12:23 -0600

    My Portfolio
    Draft copy. Still needs to be updated.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+	$('#fullpage').fullpage({
+		resize: false,
+		autoScrolling: false,
+		keyboardScrolling: false,
+		//scrollOverflow: true,
+		menu: '#menu',
+		anchors: ['aboutPage', 'portfolioPage', 'skillsPage', 'contactPage']
+		//navigation: true,
+		//navigationPosition: 'right',
+		//navigationTooltips: ['About', 'Portfolio', 'Skills', 'Contact']
+	});
+
+	$('#mobile-menu-link').on( 'click', function (e) {
+		e.preventDefault();
+		$('#menu').stop().slideToggle();
+
+	});
+	$('#menu a').on('click', function() {
+		if ( $('#mobile-menu-link').is(':visible') ) {
+			$('#menu').stop().slideToggle();
+		}
+
+	});
+	$( window ).scroll( function() {
+		if ( $('#mobile-menu-link').is(':visible') ) {
+			$('#menu').slideUp();
+		}
+	});
+
+	$(window).resize(function(){
+		if($(window).width() > 575 &&
+			!$('#mobile-menu-link').is(':visible') &&
+			!$('#menu').is(':visible') ){
+			$('#menu').show();
+		}
+	});
+
+});
